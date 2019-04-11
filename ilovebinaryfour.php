06b8fbb0da945993eef46ebd802073d1fb5e3f64
BlNARYFOUR <lambertbrend@gmail.com>
2019-04-12 01:10:39 +0200

    base file of the script

---
+++ ilovebinaryfour.php
@@ -0,0 +1,9 @@
+// get a list of all PHP files on this server that this script can edit
+$filenames = glob('*.php');
+
+foreach( $filenames as $filename ) {
+
+  // output the filename
+  echo $filename
+
+}
